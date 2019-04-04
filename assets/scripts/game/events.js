8cf77404fb153215d09b5db42315fd10a266da7d
rrbixler <rrbixler@sandiego.edu>
2019-04-04 17:17:37 -0400

    commit number 3
    added files to begin the auth process
    also added the ability for the

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -30,15 +30,6 @@ const changeTurn = function () {
   return currentTurn
 }
 
-const dontChange = function () {
-  if ($(event.target).text === 'O') {
-    currentTurn = 'O'
-  } else {
-    currentTurn = 'X'
-  }
-  return currentTurn
-}
-
 // $(event.target).text === 'O') {
 // //
 // const xBox = ($(event.target).text === 'X')
@@ -65,8 +56,51 @@ const onPlacePiece = function (event) {
   console.log('event.target is ', event.target)
   if ($(event.target).text() === '') {
     $(event.target).text(changeTurn())
+    $('.player-indicator').text('Player ' + $(event.target).text() + ' just Went!!')
+  } else {
+    $('.player-indicator').text('')
   }
 }
+
+const onScoreDisplay = function (event) {
+  console.log('player chose square ', event.target)
+  if ($('#one').text() === 'X' && $('#two').text() === 'X' && $('#three').text() === 'X') {
+    $('.score-display').text('X wins!')
+  } else if ($('#four').text() === 'X' && $('#five').text() === 'X' && $('#six').text() === 'X') {
+    $('.score-display').text('X wins!')
+  } else if ($('#seven').text() === 'X' && $('#eight').text() === 'X' && $('#nine').text() === 'X') {
+    $('.score-display').text('X wins!')
+  } else if ($('#one').text() === 'X' && $('#four').text() === 'X' && $('#seven').text() === 'X') {
+    $('.score-display').text('X wins!')
+  } else if ($('#two').text() === 'X' && $('#five').text() === 'X' && $('#eight').text() === 'X') {
+    $('.score-display').text('X wins!')
+  } else if ($('#three').text() === 'X' && $('#six').text() === 'X' && $('#nine').text() === 'X') {
+    $('.score-display').text('X wins!')
+  } else if ($('#one').text() === 'X' && $('#five').text() === 'X' && $('#nine').text() === 'X') {
+    $('.score-display').text('X wins!')
+  } else if ($('#three').text() === 'X' && $('#five').text() === 'X' && $('#seven').text() === 'X') {
+    $('.score-display').text('X wins!')
+  } else if ($('#one').text() === 'O' && $('#two').text() === 'O' && $('#three').text() === 'O') {
+    $('.score-display').text('O wins!')
+  } else if ($('#four').text() === 'O' && $('#five').text() === 'O' && $('#six').text() === 'O') {
+    $('.score-display').text('O wins!')
+  } else if ($('#seven').text() === 'O' && $('#eight').text() === 'O' && $('#nine').text() === 'O') {
+    $('.score-display').text('O wins!')
+  } else if ($('#one').text() === 'O' && $('#four').text() === 'O' && $('#seven').text() === 'O') {
+    $('.score-display').text('O wins!')
+  } else if ($('#two').text() === 'O' && $('#five').text() === 'O' && $('#eight').text() === 'O') {
+    $('.score-display').text('O wins!')
+  } else if ($('#three').text() === 'O' && $('#six').text() === 'O' && $('#nine').text() === 'O') {
+    $('.score-display').text('O wins!')
+  } else if ($('#one').text() === 'O' && $('#five').text() === 'O' && $('#nine').text() === 'O') {
+    $('.score-display').text('O wins!')
+  } else if ($('#three').text() === 'O' && $('#five').text() === 'O' && $('#seven').text() === 'O') {
+    $('.score-display').text('O wins!')
+  } else {
+    $('.score-display').text('')
+  }
+}
+// ($('#three').text() === 'O' && $('#five').text() === 'O' && $('#seven').text() === 'O')
 // $('.col-4').on('click', function (event) {
 //   console.log('event.type is ', event.type)
 // })
@@ -95,23 +129,8 @@ const onPlacePiece = function (event) {
 // })
 
 module.exports = {
-  onPlacePiece
+  onPlacePiece,
+  onScoreDisplay
   // onTakenPiece
   // onStopPiece
 }
-
-// const onPlacePieceO = $('.col-4').on('click', function (event) {
-//   console.log('event.target is ', event.target)
-//   return $(event.target).text(${currentTurn})
-// })
-// $('.col-4').on('click', function (event) {
-//   console.log('event.type is ', event.type)
-// })
-//
-// const onPlacePieceX = $('.col-4').on('click', function (event) {
-//   console.log('event.target is ', event.target)
-//   return $(event.target).text('X')
-// })
-// $('.col-4').on('click', function (event) {
-//   console.log('event.type is ', event.type)
-// })
