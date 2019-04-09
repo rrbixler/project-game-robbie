e6e4d2dd38157313a84f94a5658037f81f658124
rrbixler <rrbixler@sandiego.edu>
2019-04-09 08:33:36 -0400

    got rid of final console.logs

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -1,35 +1,33 @@
 const store = require('../store.js')
 
 const onPlacePieceSuccess = function (response) {
-  console.log('successfully placed piece to: ', response)
   store.place = response.place
 }
+
 const onCreateSuccess = function (response) {
-  console.log('successfully created a new game: ', response)
   $('.container').show()
   store.game = response.game
 }
+
 const onCreateError = function (data) {
-  console.log('didnt work', data)
 }
 
 const updateGameSuccess = function (data) {
-  console.log('successfully updated game: ', data)
   store.games = data.games
 }
 
 const updateGameError = function (data) {
-  console.log('did not work', data)
 }
 
 const onGetGamesSuccess = function (response) {
   const count = response.games.length
   $('.stats-display').text(`Youve played ${count} games!`)
 }
+
 const onGetGamesError = function (data) {
-  console.log('successfully updated game: ', data)
   store.games = data.games
 }
+
 module.exports = {
   onPlacePieceSuccess,
   onCreateSuccess,
